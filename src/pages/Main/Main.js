44a1c3e36d4a9edf56432caee4e840d199ab6141
민하늘 <onlynforever@gmail.com>
2022-01-26 00:28:44 +0900

    KAKAO LOGIN 수정

--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -8,18 +8,16 @@ import Activity from './components/Activity';
 
 const Main = () => {
 	return (
-		<>
-			<MainWrapper>
-				<MainTop>
-					<CovidInfo>에어비앤비의 코로나19 대응 방안에 대한 최신 정보를 확인하세요.</CovidInfo>
-					<Header />
-				</MainTop>
-				<MainBanner />
-				<TripIdea />
-				<Activity />
-				<Footer />
-			</MainWrapper>
-		</>
+		<MainWrapper>
+			<MainTop>
+				<CovidInfo>에어비앤비의 코로나19 대응 방안에 대한 최신 정보를 확인하세요.</CovidInfo>
+				<Header />
+			</MainTop>
+			<MainBanner />
+			<TripIdea />
+			<Activity />
+			<Footer />
+		</MainWrapper>
 	);
 };
 
