6f8d00f754c26a9007db26b29ef88bb98dd27f5b
zzangzzong92 <zzangzzong92@gmail.com>
2022-02-13 10:46:34 +0900

    edit: detail파일 주석제거, globalStyle.js body부분 가로축 hidden

--- src/pages/Detail/Detail.js
+++ src/pages/Detail/Detail.js
@@ -36,15 +36,6 @@ export default function Detail() {
 		document.body.style.cssText = `position: fixed; top: -${window.scrollY}px`;
 	};
 
-	// useEffect(() => {
-	// 	document.body.style.cssText = `position: fixed; top: -${window.scrollY}px`;
-	// 	return () => {
-	// 		const scrollY = document.body.style.top;
-	// 		document.body.style.cssText = `position: ""; top: "";`;
-	// 		window.scrollTo(0, parseInt(scrollY || '0') * -1);
-	// 	};
-	// }, []);
-
 	const handleClose = () => {
 		setShowModal(false);
 		document.body.style.cssText = `position: ""; top: "";`;
